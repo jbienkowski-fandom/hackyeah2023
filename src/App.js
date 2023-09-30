f83b1517e7e05b47d8a4af094a63759f0b1d2e15
Jakub Bieńkowski <jbienkowski@fandom.com>
2023-09-30 19:57:15 +0200

    bulma

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import './App.css';
 
 function App() {
     return (<div className="container">
-            {projects.map((project, idx) => <div key={idx} className="half-container">
-                <h1>{project.tytul}</h1>
+        <div className="columns">
+            {projects.map((project, idx) => <div key={idx} className="column is-half">
+                <div className="box">
+                    <figure className="image is-1by1">
+                        <img src={project.fotos[0]} alt="foto" />
+                    </figure>
+                    <h1 className="subtitle">{project.tytul}</h1>
+                </div>
             </div>)}
-        </div>);
+        </div>
+    </div>);
 }
 
 export default App;
